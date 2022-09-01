ff05180d2da21f1e837d4b51fd11b0ec7826cfc7
ff6347 <hash@ff6347.email>
2022-09-01 09:51:45 +0200

    feat: init first version

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	extends: "@technologiestiftung/semantic-release-config",
+	npmPublish: true,
+	// branches: [
+	//   { name: "main" },
+	//   { name: "staging", channel: "pre/rc", prerelease: "rc" }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
+	//   { name: "beta", channel: "beta", prerelease: true }, // `prerelease` is set to `beta` as it is the value of `name`
+	// ],
+	// npmPublish: false,
+	// dryRun: false,
+	// plugins: [
+	//   "@semantic-release/commit-analyzer",
+	//   "@semantic-release/release-notes-generator",
+	//   "@semantic-release/changelog",
+	//   "@semantic-release/npm",
+	//   "@semantic-release/git",
+	//   "@semantic-release/github",
+	// ],
+};
