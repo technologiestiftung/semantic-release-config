4650df9b1874d080724ccb7c069135759dfe48c1
ff6347 <hash@ff6347.email>
2022-09-01 11:27:13 +0200

    feat: Dog food better not depending on installed package

---
+++ test/config.test.js
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "vitest";
+const defaultConfig = require("../index");
+const config = require("../release.config");
+describe("basic test", () => {
+	it("should have publish false", () => {
+		expect(defaultConfig.npmPublish).equal(false);
+		expect(config.npmPublish).equal(true);
+	});
+	it("should snapshot repo config", () => {
+		expect(config).toMatchInlineSnapshot(`
+			{
+			  "branches": [
+			    {
+			      "name": "main",
+			    },
+			  ],
+			  "dryRun": false,
+			  "npmPublish": true,
+			  "plugins": [
+			    "@semantic-release/commit-analyzer",
+			    "@semantic-release/release-notes-generator",
+			    "@semantic-release/changelog",
+			    "@semantic-release/npm",
+			    "@semantic-release/git",
+			    "@semantic-release/github",
+			  ],
+			}
+		`);
+	});
+});
